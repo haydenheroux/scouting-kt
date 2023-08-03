8140817e93408cc9ae15dec4ef5fc3ae46eb04b2
Hayden Heroux <haydenheroux@gmail.com>
2023-08-03 02:45:08 -0400

    Add `new-season` route.

--- src/main/kotlin/io/github/haydenheroux/scouting/routes/API.kt
+++ src/main/kotlin/io/github/haydenheroux/scouting/routes/API.kt
@@ -2,6 +2,7 @@ package io.github.haydenheroux.scouting.routes
 
 import io.github.haydenheroux.scouting.database.db
 import io.github.haydenheroux.scouting.models.event.Event
+import io.github.haydenheroux.scouting.models.team.Season
 import io.github.haydenheroux.scouting.models.team.Team
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -21,7 +22,23 @@ fun Route.api() {
             call.respond(HttpStatusCode.fromValue(200))
         }
 
-        post("new-event") {
+        post("/new-season") {
+            val season = call.receive<Season>()
+
+            assert(season.robots.isEmpty())
+            assert(season.events.isEmpty())
+
+            val teamNumber: Int = call.request.queryParameters["team"]!!.toInt()
+            val team = db.getTeamByNumber(teamNumber)
+
+            season.team = team
+
+            db.insertSeason(season)
+
+            call.respond(HttpStatusCode.fromValue(200))
+        }
+
+        post("/new-event") {
             val event = call.receive<Event>()
 
             assert(event.matches.isEmpty())
